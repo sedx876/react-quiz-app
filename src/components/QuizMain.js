9366236a6b17aa7cff93f735fec0039e18b336ce
sedx876 <swatkins876@gmail.com>
2020-08-09 10:14:09 -0400

    Quiz Main

---
+++ src/components/QuizMain.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './QuizMain.css';
+
+
+class QuizMain extends Component {
+  state= {
+    questions: {
+      1: 'What US city is known as the birthplace of jazz?',
+      2: 'What is the capital of Greece?',
+      3: 'What planet is Superman from?'
+    },
+    answers: {
+      1: {
+        1: 'Chicago',
+        2: 'New Orleans',
+        3: 'New York'
+      },
+      2: {
+        1: 'Athens',
+        2: 'Patras',
+        3: 'Kalamata'
+      },
+      3: {
+        1: 'Krypton',
+        2: 'Mars',
+        3: 'Saturn'
+      }
+    },
+    correctAnswers: {
+      1: '2',
+      2: '1',
+      3: '1'
+    },
+    correctAnswer: 0,
+    clickedAnswer: 0,
+    step: 1,
+    score: 0
+  }
+  
+  render() {
+    return (
+      <div className="Content">
+      Hi there
+      </div>
+    );
+  }
+}
+
+export default QuizMain;
